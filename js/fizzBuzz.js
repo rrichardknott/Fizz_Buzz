b7cfe02201c6d9466dc2ad2ea73cdae97e2f1733
rrichardknott <67433117+rrichardknott@users.noreply.github.com>
2020-07-15 07:24:13 -0700

    Initial commit

---
+++ js/fizzBuzz.js
@@ -0,0 +1,35 @@
+﻿
+$("#submitButton").click(function () {    
+        
+    let emptyString = "";
+    let firstNumber = parseInt($("#firstNumber").val());    
+    let lastNumber = parseInt($("#lastNumber").val());    
+          
+    for (let i = firstNumber; i <= lastNumber; i++) {
+        if (i % 5 == 0 && i % 3 == 0) {
+            emptyString += "Fizz Buzz! \n";                              
+        }
+
+        else if (i % 5 == 0) {
+            emptyString += "Fizz \n";            
+        }
+
+        else if (i % 3 == 0) {
+            emptyString += "Buzz \n";            
+        }
+
+        else {
+            emptyString += i + '\n';            
+        }        
+    }
+    $("#resultText").val(emptyString);
+
+});
+
+$("#clearButton").on("click", function () {
+    $("#firstNumber").val("");
+    $("#lastNumber").val("");
+    $("#resultText").val("");
+    $("#firstNumber").focus();
+});
+
